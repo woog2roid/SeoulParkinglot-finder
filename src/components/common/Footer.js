e6526bc73a4dd09d51a6a810d5b8a40fb776378e
woog2roid <wooguijung@korea.ac.kr>
2021-07-23 14:57:36 +0000

    design(Footer): Footer 폰트크기 수정

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Wrapper = styled.div`
-    width: 100%;
-	font-weight: light;
-`;
 const Profile = styled.div`
     padding: 5px;
     text-align: center;
 	@media all and (max-width:767px) {
-        font-size: 13px;
+        font-size: 10px;
     }
     @media all and (min-width:768px) and (max-width:1080px) {
-        font-size: 15px;
+        font-size: 13px;
     }
     @media all and (min-width:1081px) {
-        font-size: 18px;
+        font-size: 15px;
     }
 `;
 
 const Footer = () => {
     return (
-        <Wrapper>
-            <Profile>
-                © 2021 woog2roid. All rights reserved.
-            </Profile>
-        </Wrapper>
+        <Profile>
+            © 2021 woog2roid. All rights reserved.
+        </Profile>
     );
 };
+
 export default Footer;
