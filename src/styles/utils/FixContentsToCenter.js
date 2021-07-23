70fb73dd235c6b17b4e64d35cc781a29202d743b
woog2roid <wooguijung@korea.ac.kr>
2021-07-23 14:48:36 +0000

    feat(utils): about과 404 페이지 등 컨텐츠 컴포넌트를 페이지 정중앙에 위치시키는 역할을 하는 FixContentsToCenter 작성

---
+++ src/styles/utils/FixContentsToCenter.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+	text-align: center;
+	position: absolute;
+	height: 20%;
+	top: 0;
+	bottom: 0;
+	left: 0;
+	right: 0;
+	margin: auto;
+`;
