6f4e665419425979757f3cba87ebc621a11eed6a
woog2roid <wooguijung@korea.ac.kr>
2021-08-13 13:50:43 +0000

    feat(modals+common/map): map 제작 및, zoomlevel을 관리하는 모달 완성

--- src/components/common/map/Map.js
+++ src/components/common/map/Map.js
@@ -1,21 +1,24 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
+import ZoomLevelContext from '../../../contexts/ZoomLevelContext';
 
 const Map = () => {
 	const map = useRef(null);
-
+	const { state } = useContext(ZoomLevelContext);
+	
 	useEffect(() => {
 		window.kakao.maps.load(() => {
 			navigator.geolocation.getCurrentPosition((position) => {
 			const lat=position.coords.latitude,
 				  lng=position.coords.longitude;
+			//37.5,127
 			const options = {
 				center: new window.kakao.maps.LatLng(lat, lng),
-				level: 7,
+				level: state.level,
 			};
 				new window.kakao.maps.Map(map.current, options);
 			});
 		});
-	}, []);
+	}, [state]);
 	
 
 	return (
--- src/components/common/nav/modals/LevelControl.js
+++ src/components/common/nav/modals/LevelControl.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import {
-	Modal,
-	ModalHeader,
-	ModalBody,
-	Button,
-} from 'reactstrap';
+import React, { useContext } from 'react';
+import { Modal, ModalHeader, ModalBody, Button } from 'reactstrap';
 import { ButtonWrapper } from './LevelControlStyle';
 import '../../../../global_styles/bootstrap_custom/modal.css';
-import Map from '../../map/Map'
+import Map from '../../map/Map';
+import ZoomLevelContext from '../../../../contexts/ZoomLevelContext';
 
 const LevelControl = ({ isOpen, toggle }) => {
+	const { actions } = useContext(ZoomLevelContext);
+
+	const onClickSml = () => {
+		actions.setLevel(5);
+	};
+	const onClickStd = () => {
+		actions.setLevel(6);
+	};
+	const onClickBig = () => {
+		actions.setLevel(8);
+	};
+
 	return (
 		<Modal isOpen={isOpen}>
-			<ModalHeader toggle={toggle}>
-				지도 축소/확대 레벨 
-			</ModalHeader>
+			<ModalHeader toggle={toggle}>지도 축소/확대 레벨</ModalHeader>
 			<ModalBody>
 				<ButtonWrapper>
-    				<Button outline color="primary">좁게</Button>
-    				<Button outline color="primary">표준</Button>
-				    <Button outline color="primary">넓게</Button>
-    			</ButtonWrapper>
+					<Button outline color="primary" onClick={onClickSml}>
+						좁게
+					</Button>
+					<Button outline color="primary" onClick={onClickStd}>
+						표준
+					</Button>
+					<Button outline color="primary" onClick={onClickBig}>
+						넓게
+					</Button>
+				</ButtonWrapper>
 				<Map />
 			</ModalBody>
 		</Modal>
